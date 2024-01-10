91713551918b83df43c891a9c3e521dfc00f665b
farawell <farawell777@kaist.ac.kr>
2024-01-10 23:39:08 +0900

    .

---
+++ src/wanderbot/src/red_light_green_light.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#ROS1
+
+import rospy
+from geometry_msgs.msg import Twist
+
+cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1) # only buffer 1 message
+rospy.init_node('red_light_green_light')
+
+red_light_twist = Twist() # set all the velocity subcomponents to 0, telling robot to stop.
+green_light_twist = Twist()
+green_light_twist.linear.x = 0.5 # "Go straight 0.5m/s"
+
+driving_forward = False
+light_change_time = rospy.Time.now()
+rate = rospy.Rate(10)
+
+while not rospy.is_shutdown():
+    if driving_forward:
+        cmd_vel_pub.publish(green_light_twist)
+    else:
+        cmd_vel_pub.publish(red_light_twist)
+    
+    if light_change_time > rospy.Time.now():
+        driving_forward = not driving_forward
+        light_change_time = rospy.Time.now() + rospy.Duration(3)
+    
+    rate.sleep(10)
