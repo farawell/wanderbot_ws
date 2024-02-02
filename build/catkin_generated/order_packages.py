a6be2aeaa8094208edcc970e63b3bae3589fd73a
farawell <farawell777@kaist.ac.kr>
2024-02-02 23:33:44 +0900

    Handling some untracked files

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/rirolab/wanderbot_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/rirolab/catkin_ws/devel;/home/rirolab/quadruped_ws/devel;/opt/ros/noetic'.split(';') if '/home/rirolab/catkin_ws/devel;/home/rirolab/quadruped_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/rirolab/quadruped_ws/devel;/opt/ros/noetic'.split(';') if '/home/rirolab/quadruped_ws/devel;/opt/ros/noetic' != '' else []
